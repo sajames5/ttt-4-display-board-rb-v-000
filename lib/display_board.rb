1950da5c821b89c7e264271f3bfc4d7088eb4e1a
Stephanie <s.a.james5@gmail.com>
2018-03-22 17:34:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 def display_board(rows)
-  puts "#{rows[0]}   |   |   "
-  puts "#{rows[1]}-----------"
-  puts "#{rows[2]}   |   |   "
-  puts "#{rows[3]}-----------"
-  puts "#{rows[4]}   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end 
 
 
